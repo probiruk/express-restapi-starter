8f83b774218c42f7353f7b2f8256618c1d52b4f6
Biruk Erjamo <birukdotwork@gmail.com>
2023-04-30 22:50:57 +0300

    feat: Setup the project with eslint and prettier
    Signed-off-by: Biruk Erjamo <birukdotwork@gmail.com>

--- helpers/logger.ts
+++ helpers/logger.ts
@@ -1,26 +1,27 @@
-import { createLogger, format, transports } from 'winston'
-import DailyRotateFile from 'winston-daily-rotate-file'
+import { createLogger, format, transports } from "winston";
+import DailyRotateFile from "winston-daily-rotate-file";
 
 const logFormat = format.printf(
-    ({ level, message, timestamp, stack }) =>
-        `${timestamp} ${level}: ${stack || message}`
-)
+  ({ level, message, timestamp, stack }) =>
+    // eslint-disable-next-line @typescript-eslint/restrict-template-expressions, @typescript-eslint/strict-boolean-expressions
+    `${timestamp} ${level}: ${stack || message}`
+);
 
 const transport = new DailyRotateFile({
-    filename: './logs/server-%DATE%.log',
-    datePattern: 'YYYY-MM-DD-HH',
-    maxSize: '20m',
-    maxFiles: '14d',
-    zippedArchive: true,
-})
+  filename: "./logs/server-%DATE%.log",
+  datePattern: "YYYY-MM-DD-HH",
+  maxSize: "20m",
+  maxFiles: "14d",
+  zippedArchive: true,
+});
 
 const logger = createLogger({
-    format: format.combine(
-        format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-        format.errors({ stack: true }),
-        logFormat
-    ),
-    transports: [transport, new transports.Console()],
-})
+  format: format.combine(
+    format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    format.errors({ stack: true }),
+    logFormat
+  ),
+  transports: [transport, new transports.Console()],
+});
 
-export default logger
+export default logger;
--- main.ts
+++ main.ts
@@ -1,24 +1,24 @@
-import express from "express"
-import helmet from "helmet"
-import cors from "cors"
-import morgan from "morgan"
-import dotenv from "dotenv"
-dotenv.config()
+import express from "express";
+import helmet from "helmet";
+import cors from "cors";
+import morgan from "morgan";
+import dotenv from "dotenv";
 
-import logger from "./helpers/logger"
+import logger from "./helpers/logger";
+dotenv.config();
 
 // Enviroment variables
-const NODE_ENV = process.env.NODE_ENV || "dev"
-const PORT = Number(process.env.PORT)
+const NODE_ENV = process.env?.NODE_ENV != null ? process.env?.NODE_ENV : "dev";
+const PORT = Number(process.env.PORT);
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(helmet())
-app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }))
-app.use(cors())
-app.use(morgan(NODE_ENV == 'prod' ? 'combined' : 'dev'))
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(cors());
+app.use(morgan(NODE_ENV === "prod" ? "combined" : "dev"));
 
 app.listen(PORT, () => {
-    logger.info(`Server started on port ${PORT}`)
-})
+  logger.info(`Server started on port ${PORT}`);
+});
